c1ca180511d941a391ca50a6c811a777cc1abf58
Your Name <er.kalai.v@gmail.com>
2025-03-22 19:58:16 +0530

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Calc from './components/Calc'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Calc />
+     <Analytics />
     </>
   )
 }
